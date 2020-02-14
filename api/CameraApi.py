e762b2e73063c0ee28b6e13eaeb89a2c33ddf97b
Enrico Meloni <enrico.meloni@outlook.it>
2020-02-14 16:54:22 +0100

    Add api for optical flow

--- api/CameraApi.py
+++ api/CameraApi.py
@@ -15,15 +15,17 @@ class CameraApi(Api):
     def get_object_frame(self):
         return self.call("object", Method.GET)
 
-    def __init__(self, object_frame_active: bool = True, category_frame_active: bool = True):
+    def __init__(self, object_frame_active: bool = True, category_frame_active: bool = True, flow_frame_active: bool = True):
         super().__init__()
         self.object_frame_active : bool = object_frame_active
         self.category_frame_active: bool = category_frame_active
+        self.flow_frame_active: bool = flow_frame_active
 
     def get_frame(self):
         params = {
             "Main": True,
             "Category": self.category_frame_active,
-            "Object": self.object_frame_active
+            "Object": self.object_frame_active,
+            "Optical": self.flow_frame_active
         }
         return self.call("frame", Method.GET, data=params)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import base64
+import time
 
 import numpy as np
 
@@ -20,20 +21,33 @@ def decode_image(b64_input):
 
 
 def main():
-    camera_api = CameraApi()
+    camera_api = CameraApi(object_frame_active=False, category_frame_active=True)
 
+    last_unity_time = 0
     while True:
+        start_real_time = time.time()
+        start_unity_time = last_unity_time
         resp = camera_api.get_frame()
         images = resp["Content"]
         main_img = decode_image(images["Main"])
-        cat_img = decode_image(images["Category"])
-        obj_img = decode_image(images["Object"])
-
         cv2.imshow("main", main_img)
-        cv2.imshow("cat", cat_img)
-        cv2.imshow("obj", obj_img)
 
+        if camera_api.category_frame_active:
+            cat_img = decode_image(images["Category"])
+            cv2.imshow("cat", cat_img)
+
+        if camera_api.object_frame_active:
+            obj_img = decode_image(images["Object"])
+            cv2.imshow("obj", obj_img)
+
+        if camera_api.flow_frame_active:
+            flow_img = decode_image(images["Optical"])
+            cv2.imshow("flow", flow_img)
+
+        last_unity_time = resp["Content"]["Frame"]
         key = cv2.waitKey(10)
+        print(f"real elapsed time: {time.time() - start_real_time}")
+        print(f"unity time: {last_unity_time} . elapsed: {last_unity_time - start_unity_time}")
         if key == 27:  # ESC Pressed
             return
 
