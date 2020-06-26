bb00c47c2990bbb0c8677b1d678bbb2e5ce65d62
enricomeloni <enrico-meloni@hotmail.it>
2020-06-26 18:51:10 +0200

    Change name to SAILenv and add Matteo to copyright notices

--- example_unity_client.py
+++ example_unity_client.py
@@ -1,9 +1,9 @@
 #
-# Copyright (C) 2020 Enrico Meloni, Luca Pasqualini
+# Copyright (C) 2020 Enrico Meloni, Luca Pasqualini, Matteo Tiezzi, Matteo Tiezzi
 # University of Siena - Artificial Intelligence Laboratory - SAILab
 #
 #
-# L2S is licensed under a MIT license.
+# SAILenv is licensed under a MIT license.
 #
 # You should have received a copy of the license along with this
 # work. If not, see <https://en.wikipedia.org/wiki/MIT_License>.
