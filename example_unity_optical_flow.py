26a61f191fbb56e9d5d7f55859458de7d8b2f68b
mtiezzi <tiezzi7matteo@gmail.com>
2020-06-28 17:09:58 +0200

    added rebuild flag to show that is the rubuilding that alter the optical flow plot

--- example_unity_optical_flow.py
+++ example_unity_optical_flow.py
@@ -13,28 +13,29 @@ toMax = 1
 def draw_flow_map(optical_flow):
     hsv = np.zeros((optical_flow.shape[0], optical_flow.shape[1], 3), dtype=np.uint8)
     hsv[..., 1] = 255
-    mag, ang = cv2.cartToPolar(optical_flow[..., 0], optical_flow[..., 1])
 
+    mag, ang = cv2.cartToPolar(optical_flow[..., 0], optical_flow[..., 1])
     hsv[..., 0] = ang * 180 / np.pi / 2
     hsv[..., 2] = cv2.normalize(mag, None, 0, 255, cv2.NORM_MINMAX)
-    #print(np.min(hsv), np.max(hsv))
     frame_flow_map = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
 
     return frame_flow_map
 
-
 if __name__ == "__main__":
     print("Generating agent...")
-    agent = SocketAgent(flow_frame_active=True, object_frame_active=False, main_frame_active=True,
-                  category_frame_active=False, width=200, height=150, host="127.0.0.1", port=8085)
+    agent = SocketAgent(depth_frame_active=False,
+                  flow_frame_active=True,
+                  object_frame_active=False,
+                  main_frame_active=True,
+                  category_frame_active=True, width=256, height=192, host="localhost", port=8085, gzip=False)
     print("Registering agent on server...")
     agent.register()
     print(f"Agent registered with ID: {agent.id}")
     last_unity_time: float = 0.0
-
+    rebuild_flag = True
     print(f"Available scenes: {agent.scenes}")
 
-    scene = agent.scenes[0]
+    scene = agent.scenes[2]
     print(f"Changing scene to {scene}")
     agent.change_scene(scene)
     try:
@@ -46,7 +47,7 @@ if __name__ == "__main__":
             frame = agent.get_frame()
             current_main = frame["main"]
             of = frame["flow"]
-            print(np.min(of), np.max(of))
+            #print(np.min(of), np.max(of))
             rebuilt_main = np.zeros(current_main.shape, current_main.dtype)
             #rebuilt_main = np.copy(current_main)
             h, w, c = current_main.shape
@@ -56,22 +57,22 @@ if __name__ == "__main__":
             flow_img = draw_flow_map(of)
             cv2.imshow("Optical Flow", flow_img)
             # of = 3 * of
+            if rebuild_flag:
+                for j1 in range(0, w):
+                    for i1 in range(0, h):
+                        if np.all(of[i1][j1] == [0,0]):
+                            continue
+                        i0 = max(min(int(round(i1 + of[i1][j1][1])), h - 1), 0)
+                        j0 = max(min(int(round(j1 + of[i1][j1][0])), w - 1), 0)
+                        rebuilt_main[i0][j0] = current_main[i1][j1]
+                        # rebuilt_main[i1][j1] = prev_main[i1][j1]
 
-            for j1 in range(0, w):
-                for i1 in range(0, h):
-                    if np.all(of[i1][j1] == [0,0]):
-                        continue
-                    i0 = max(min(int(round(i1 + of[i1][j1][1])), h - 1), 0)
-                    j0 = max(min(int(round(j1 + of[i1][j1][0])), w - 1), 0)
-                    rebuilt_main[i0][j0] = current_main[i1][j1]
-                    # rebuilt_main[i1][j1] = prev_main[i1][j1]
-
-            cv2.imshow("Previous", prev_main)
-            cv2.imshow("Rebuilt", rebuilt_main)
-            cv2.imshow("Current", current_main)
+                cv2.imshow("Previous", prev_main)
+                cv2.imshow("Rebuilt", rebuilt_main)
+                cv2.imshow("Current", current_main)
 
             prev_main = current_main
-            key = cv2.waitKey(100)
+            key = cv2.waitKey(1)
             print(f"OF ({of.min()}, {of.max()})")
             print(f"FPS: {1 / (time.time() - start_real_time)}")
             if key == 27:  # ESC Pressed
